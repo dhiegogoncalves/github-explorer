f7c5221306b401d0782d24e7855ccf770f397827
Dhiego <dhhiego@gmail.com>
2020-05-19 23:32:58 -0400

    Implementação do layout da tela dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,68 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/6399309?s=460&u=23482a8359a1f0b47531b28a0e8cbb932186d509&v=4"
+            alt="Dhiego"
+          />
+          <div>
+            <strong>repository/form</strong>
+            <p>teste.....</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/6399309?s=460&u=23482a8359a1f0b47531b28a0e8cbb932186d509&v=4"
+            alt="Dhiego"
+          />
+          <div>
+            <strong>repository/form</strong>
+            <p>teste.....</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/6399309?s=460&u=23482a8359a1f0b47531b28a0e8cbb932186d509&v=4"
+            alt="Dhiego"
+          />
+          <div>
+            <strong>repository/form</strong>
+            <p>teste.....</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/6399309?s=460&u=23482a8359a1f0b47531b28a0e8cbb932186d509&v=4"
+            alt="Dhiego"
+          />
+          <div>
+            <strong>repository/form</strong>
+            <p>teste.....</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
